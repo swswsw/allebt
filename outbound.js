a83db8d2cf44e329706dd1cdda116f2170644fb6
sol wu <apiswswsw@gmail.com>
2018-10-06 04:12:02 -0700

    file

---
+++ outbound.js
@@ -0,0 +1,17 @@
+// Download the Node helper library from twilio.com/docs/node/install
+// These identifiers are your accountSid and authToken from
+// https://www.twilio.com/console
+//const accountSid = 'AC20ba23fb4117ebd66dd710e2305d9943'; // test
+//const authToken = 'ba5ebd75817c1f33cfdc0bf534f2195f'; // test
+const accountSid = 'AC54c2dc42f931abf1d798dece18ed5591'; // live 
+const authToken = '2ff75a5f3b9127bc745fd6eec8129524'; // live
+const client = require('twilio')(accountSid, authToken);
+
+client.calls
+      .create({
+         url: 'http://demo.twilio.com/docs/voice.xml',
+         to: '+16505333392',
+         from: '+14153407736'
+       })
+      .then(call => console.log(call.sid))
+      .done();
---
+++ outbound2.js
@@ -0,0 +1,28 @@
+// Download the Node helper library from twilio.com/docs/node/install
+// These identifiers are your accountSid and authToken from
+// https://www.twilio.com/console
+//const accountSid = 'AC20ba23fb4117ebd66dd710e2305d9943'; // test
+//const authToken = 'ba5ebd75817c1f33cfdc0bf534f2195f'; // test
+const accountSid = 'AC54c2dc42f931abf1d798dece18ed5591'; // live 
+const authToken = '2ff75a5f3b9127bc745fd6eec8129524'; // live
+var twilio = require('twilio');
+var client = new twilio(accountSid, authToken);
+
+//var VoiceResponse = client.twiml.VoiceResponse;
+const VoiceResponse = require('twilio').twiml.VoiceResponse;
+
+
+const response = new VoiceResponse();
+response.dial('+16505333392');
+response.say('Goodbye');
+
+console.log(response.toString());
+
+// client.calls
+//       .create({
+//          url: response.toString(),
+//          to: '+16505333392',
+//          from: '+14153407736'
+//        })
+//       .then(call => console.log(call.sid))
+//       .done();
