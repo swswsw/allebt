413b558c407e4b70d7945cb14daf60d06e150f61
sol wu <apiswswsw@gmail.com>
2018-10-06 06:13:57 -0700

    test

--- outbound3.js
+++ outbound3.js
@@ -3,8 +3,8 @@
 // https://www.twilio.com/console
 //const accountSid = 'AC20ba23fb4117ebd66dd710e2305d9943'; // test
 //const authToken = 'ba5ebd75817c1f33cfdc0bf534f2195f'; // test
-const accountSid = 'AC54c2dc42f931abf1d798dece18ed5591'; // live 
-const authToken = '2ff75a5f3b9127bc745fd6eec8129524'; // live
+const accountSid = 'AC85ef88e084fba67f8e0481807f464f31'; // live 
+const authToken = '941949f8bf57af4587f0d433c98fede1'; // live
 const client = require('twilio')(accountSid, authToken);
 
 client.calls
@@ -12,7 +12,7 @@ client.calls
          record: true,
          url: 'http://raw.githubusercontent.com/swswsw/allebt/master/outbound2.xml',
          to: '+14083726477',
-         from: '+14153012420'
+         from: '+14159961780'
        })
       .then(call => console.log(call.sid))
       .done();
